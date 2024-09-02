493955e3a928b41d87ef646f406e2e1ea3647852
evawyy <evawyy@12345>
2024-09-02 14:56:44 +0800

    update, add neorg, modify cmp

--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,60 +1,61 @@
 return {
-  "nvim-neorg/neorg",
-  ft = "norg",
-  cmd = "Neorg",
-  version = "*",
-  config = function()
-    require("neorg").setup {
-      load = {
-        ["core.defaults"] = {},
-        ["core.summary"] = {
-          config={
-            strategy = "default"
-          }
-        },
-        ["core.concealer"] = {
-          config = {
-            icons = {
-              heading={
-                icons={
-                  "☘",
-                  "󰄛",
-                  "✿",
-                  "",
-                  "󱁕",
-                  "󰧱",
-                },
-              },
-              ordered={
-                icons={
-                  "1.",
-                  "1)",
-                  "A.",
-                  "A)",
-                  "a.",
-                  "a)",
-                },
-              },
-            }
-          }
-        },
-        ["core.dirman"] = {
-          config = {
-            workspaces = {
-              notes = "~/notes",
-            },
-            default_workspace = "notes",
-          },
-        },
-        ["core.qol.todo_items"] = {
-          config={
-            create_todo_items = true,
-            create_todo_parents = true,
-          }
-        }
-      },
-    }
-    vim.wo.foldlevel = 99
-    vim.wo.conceallevel = 2
-  end,
+	"nvim-neorg/neorg",
+	ft = "norg",
+	cmd = "Neorg",
+	version = "*",
+	config = function()
+		require("neorg").setup({
+			load = {
+				["core.defaults"] = {},
+				["core.completion"] = {
+					config = {
+						engine = "nvim-cmp",
+					},
+				},
+				["core.summary"] = {},
+				["core.concealer"] = {
+					config = {
+						icons = {
+							heading = {
+								icons = {
+									"☘",
+									"󰄛",
+									"✿",
+									"",
+									"󱁕",
+									"󰧱",
+								},
+							},
+							ordered = {
+								icons = {
+									"1.",
+									"1)",
+									"A.",
+									"A)",
+									"a.",
+									"a)",
+								},
+							},
+						},
+					},
+				},
+				["core.dirman"] = {
+					config = {
+						workspaces = {
+							notes = "~/notes",
+						},
+						default_workspace = "notes",
+					},
+				},
+				["core.qol.todo_items"] = {
+					config = {
+						create_todo_items = true,
+						create_todo_parents = true,
+					},
+				},
+			},
+		})
+		vim.wo.foldlevel = 99
+		vim.wo.conceallevel = 2
+	end,
 }
