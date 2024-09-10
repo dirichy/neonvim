1ebdd3fccc83990ce3164439a07b1f7d21ece5fb
evawyy <evawyy@12345>
2024-09-10 22:38:38 +0800

    update

---
+++ lua/plugins/chinese.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"rainzm/flash-zh.nvim",
+		dependencies = "folke/flash.nvim",
+		keys = { "s" },
+		config = function()
+			require("mapper").map_keymap({ "n", "x", "o" }, "s", function()
+				require("flash-zh").jump({ chinese_only = false })
+			end, {
+				desc = "Jump with flypy",
+				condition = function()
+					return vim.g.language == "zh"
+				end,
+				priority = 200,
+			})
+		end,
+	},
+}
